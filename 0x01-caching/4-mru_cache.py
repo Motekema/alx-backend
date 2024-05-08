ba36cb153001ab17ebaccb2ef6a708f27387b2ed
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-05-08 13:37:39 +0200

    Update 4-mru_cache.py

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,60 +1,39 @@
 #!/usr/bin/python3
 """ MRU Cache Module """
 
+from collections import OrderedDict
+
 from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """ MRU Cache class """
-
+    """Represents an object that allows storing and
+    retrieving items from a dictionary with an MRU
+    removal mechanism when the limit is reached.
+    """
     def __init__(self):
-        """ Initialize MRU Cache """
+        """Initializes the cache.
+        """
         super().__init__()
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """ Add an item to the cache """
+        """Adds an item in the cache.
+        """
         if key is None or item is None:
             return
-
-        if len(self.cache_data) >= self.MAX_ITEMS:
-            mru_key = max(self.cache_data, key=self.cache_data.get)
-            del self.cache_data[mru_key]
-            print("DISCARD:", mru_key)
-
-        self.cache_data[key] = item
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                mru_key, _ = self.cache_data.popitem(False)
+                print("DISCARD:", mru_key)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
 
     def get(self, key):
-        """ Retrieve an item from the cache """
-        if key is None or key not in self.cache_data:
-            return None
-
-        return self.cache_data[key]
-
-
-if __name__ == "__main__":
-    my_cache = MRUCache()
-    my_cache.put("A", "Hello")
-    my_cache.put("B", "World")
-    my_cache.put("C", "Holberton")
-    my_cache.put("D", "School")
-    my_cache.print_cache()
-    print(my_cache.get("B"))
-    my_cache.put("E", "Battery")
-    my_cache.print_cache()
-    my_cache.put("C", "Street")
-    my_cache.print_cache()
-    print(my_cache.get("A"))
-    print(my_cache.get("B"))
-    print(my_cache.get("C"))
-    my_cache.put("F", "Mission")
-    my_cache.print_cache()
-    my_cache.put("G", "San Francisco")
-    my_cache.print_cache()
-    my_cache.put("H", "H")
-    my_cache.print_cache()
-    my_cache.put("I", "I")
-    my_cache.print_cache()
-    my_cache.put("J", "J")
-    my_cache.print_cache()
-    my_cache.put("K", "K")
-    my_cache.print_cache()
+        """Retrieves an item by key.
+        """
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
