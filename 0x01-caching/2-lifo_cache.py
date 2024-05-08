1904bb99b813db7390d81c75516f26c74c7ccc49
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-05-08 13:30:33 +0200

    Update 2-lifo_cache.py

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -5,44 +5,43 @@ from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-        """ LIFO Cache class """
+    """ LIFO Cache class """
 
-            def __init__(self):
-                        """ Initialize LIFO Cache """
-                                super().__init__()
+    def __init__(self):
+        """ Initialize LIFO Cache """
+        super().__init__()
 
-                                    def put(self, key, item):
-                                            """ Add an item to the cache """
-                                                    if key is None or item is None:
-                                                                return
+    def put(self, key, item):
+        """ Add an item to the cache """
+        if key is None or item is None:
+            return
 
-                                                                    if len(self.cache_data) >= self.MAX_ITEMS:
-                                                                                    discarded_key = next(reversed(self.cache_data))
-                                                                                                del self.cache_data[discarded_key]
-                                                                                                            print("DISCARD:", discarded_key)
+        if len(self.cache_data) >= self.MAX_ITEMS:
+            discarded_key = next(reversed(self.cache_data))
+            del self.cache_data[discarded_key]
+            print("DISCARD:", discarded_key)
 
-                                                                                                                    self.cache_data[key] = item
+        self.cache_data[key] = item
 
-                                                                                                                        def get(self, key):
-                                                                                                                                """ Retrieve an item from the cache """
-                                                                                                                                        if key is None or key not in self.cache_data:
-                                                                                                                                                    return None
-                                                                                                                                                            return self.cache_data[key]
+    def get(self, key):
+        """ Retrieve an item from the cache """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
 
 
-                                                                                                                                                            if __name__ == "__main__":
-                                                                                                                                                                my_cache = LIFOCache()
-                                                                                                                                                                    my_cache.put("A", "Hello")
-                                                                                                                                                                        my_cache.put("B", "World")
-                                                                                                                                                                            my_cache.put("C", "Holberton")
-                                                                                                                                                                                my_cache.put("D", "School")
-                                                                                                                                                                                    my_cache.print_cache()
-                                                                                                                                                                                        my_cache.put("E", "Battery")
-                                                                                                                                                                                            my_cache.print_cache()
-                                                                                                                                                                                                my_cache.put("C", "Street")
-                                                                                                                                                                                                    my_cache.print_cache()
-                                                                                                                                                                                                        my_cache.put("F", "Mission")
-                                                                                                                                                                                                            my_cache.print_cache()
-                                                                                                                                                                                                                my_cache.put("G", "San Francisco")
-                                                                                                                                                                                                                    my_cache.print_cache()
-
+if __name__ == "__main__":
+    my_cache = LIFOCache()
+    my_cache.put("A", "Hello")
+    my_cache.put("B", "World")
+    my_cache.put("C", "Holberton")
+    my_cache.put("D", "School")
+    my_cache.print_cache()
+    my_cache.put("E", "Battery")
+    my_cache.print_cache()
+    my_cache.put("C", "Street")
+    my_cache.print_cache()
+    my_cache.put("F", "Mission")
+    my_cache.print_cache()
+    my_cache.put("G", "San Francisco")
+    my_cache.print_cache()
