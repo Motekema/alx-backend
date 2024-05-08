01442e8e999cf0f6e4fcaabc8f85514c52a49934
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-05-08 13:40:55 +0200

    Update 100-lfu_cache.py

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,77 +1,73 @@
 #!/usr/bin/python3
 """ LFU Cache Module """
 
+from collections import OrderedDict
+
 from base_caching import BaseCaching
-from collections import defaultdict
 
 
 class LFUCache(BaseCaching):
-    """ LFU Cache class """
-
+    """ Represents an object that allows storing and
+    retrieving items from a dictionary with an LFU
+    removal mechanism when the limit is reached.
+    """
     def __init__(self):
-        """ Initialize LFU Cache """
+        """ Initializes the cache.
+        """
         super().__init__()
-        self.frequency = defaultdict(int)
+        self.cache_data = OrderedDict()
+        self.keys_freq = []
+
+    def __reorder_items(self, mru_key):
+        """ Reorder the items in this cache based on the most
+        recently used item.
+        """
+        max_positions = []
+        mru_freq = 0
+        mru_pos = 0
+        ins_pos = 0
+        for i, key_freq in enumerate(self.keys_freq):
+            if key_freq[0] == mru_key:
+                mru_freq = key_freq[1] + 1
+                mru_pos = i
+                break
+            elif len(max_positions) == 0:
+                max_positions.append(i)
+            elif key_freq[1] < self.keys_freq[max_positions[-1]][1]:
+                max_positions.append(i)
+        max_positions.reverse()
+        for pos in max_positions:
+            if self.keys_freq[pos][1] > mru_freq:
+                break
+            ins_pos = pos
+        self.keys_freq.pop(mru_pos)
+        self.keys_freq.insert(ins_pos, [mru_key, mru_freq])
 
     def put(self, key, item):
-        """ Add an item to the cache """
+        """ Adds an item in the cache.
+        """
         if key is None or item is None:
             return
-
-        if len(self.cache_data) >= self.MAX_ITEMS:
-            min_frequency = min(self.frequency.values())
-            lfu_keys = [k for k, v in self.frequency.items() if v == min_frequency]
-            lru_key = min(lfu_keys, key=lambda k: self.cache_data.get(k))
-            del self.cache_data[lru_key]
-            del self.frequency[lru_key]
-            print("DISCARD:", lru_key)
-
-        self.cache_data[key] = item
-        self.frequency[key] += 1
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                lfu_key, _ = self.keys_freq[-1]
+                self.cache_data.pop(lfu_key)
+                self.keys_freq.pop()
+                print("DISCARD:", lfu_key)
+            self.cache_data[key] = item
+            ins_index = len(self.keys_freq)
+            for i, key_freq in enumerate(self.keys_freq):
+                if key_freq[1] == 0:
+                    ins_index = i
+                    break
+            self.keys_freq.insert(ins_index, [key, 0])
+        else:
+            self.cache_data[key] = item
+            self.__reorder_items(key)
 
     def get(self, key):
-        """ Retrieve an item from the cache """
-        if key is None or key not in self.cache_data:
-            return None
-
-        self.frequency[key] += 1
-        return self.cache_data[key]
-
-
-if __name__ == "__main__":
-    my_cache = LFUCache()
-    my_cache.put("A", "Hello")
-    my_cache.put("B", "World")
-    my_cache.put("C", "Holberton")
-    my_cache.put("D", "School")
-    my_cache.print_cache()
-    print(my_cache.get("B"))
-    my_cache.put("E", "Battery")
-    my_cache.print_cache()
-    my_cache.put("C", "Street")
-    my_cache.print_cache()
-    print(my_cache.get("A"))
-    print(my_cache.get("B"))
-    print(my_cache.get("C"))
-    my_cache.put("F", "Mission")
-    my_cache.print_cache()
-    my_cache.put("G", "San Francisco")
-    my_cache.print_cache()
-    my_cache.put("H", "H")
-    my_cache.print_cache()
-    my_cache.put("I", "I")
-    my_cache.print_cache()
-    print(my_cache.get("I"))
-    print(my_cache.get("H"))
-    print(my_cache.get("I"))
-    print(my_cache.get("H"))
-    print(my_cache.get("I"))
-    print(my_cache.get("H"))
-    my_cache.put("J", "J")
-    my_cache.print_cache()
-    my_cache.put("K", "K")
-    my_cache.print_cache()
-    my_cache.put("L", "L")
-    my_cache.print_cache()
-    my_cache.put("M", "M")
-    my_cache.print_cache()
+        """Retrieves an item by key.
+        """
+        if key is not None and key in self.cache_data:
+            self.__reorder_items(key)
+        return self.cache_data.get(key, None)
