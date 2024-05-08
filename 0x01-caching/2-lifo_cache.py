45110b65db492881dd0ea64af79b9067b321f65e
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-05-08 13:31:52 +0200

    Update 2-lifo_cache.py

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,47 +1,35 @@
 #!/usr/bin/python3
 """ LIFO Cache Module """
 
+from collections import OrderedDict
+
 from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """ LIFO Cache class """
-
+    """ Represent an object that allows storing and
+    retrieving items from a dictionary with a LIFO
+    removal mechanism when the limit is reached.
+    """
     def __init__(self):
-        """ Initialize LIFO Cache """
+        """ Initialize the cache.
+        """
         super().__init__()
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """ Add an item to the cache """
+        """ Add an item in the cache.
+        """
         if key is None or item is None:
             return
-
-        if len(self.cache_data) >= self.MAX_ITEMS:
-            discarded_key = next(reversed(self.cache_data))
-            del self.cache_data[discarded_key]
-            print("DISCARD:", discarded_key)
-
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                last_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", last_key)
         self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=True)
 
     def get(self, key):
-        """ Retrieve an item from the cache """
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data[key]
-
-
-if __name__ == "__main__":
-    my_cache = LIFOCache()
-    my_cache.put("A", "Hello")
-    my_cache.put("B", "World")
-    my_cache.put("C", "Holberton")
-    my_cache.put("D", "School")
-    my_cache.print_cache()
-    my_cache.put("E", "Battery")
-    my_cache.print_cache()
-    my_cache.put("C", "Street")
-    my_cache.print_cache()
-    my_cache.put("F", "Mission")
-    my_cache.print_cache()
-    my_cache.put("G", "San Francisco")
-    my_cache.print_cache()
+        """Retrieve an item by key.
+        """
+        return self.cache_data.get(key, None)
